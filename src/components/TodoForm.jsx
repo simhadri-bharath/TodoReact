51d8f57daed5acbc2896c5ddec796706bd68d6f9
simhadri-bharath <simhadribharath2004@gmail.com>
2024-10-19 11:54:36 +0530

    aligned files properly

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import { useTodo } from '../contexts/TodoContext';
 
 function TodoForm() {
-    const [todo,setTodo]=useState("");
-    const {addTodo}=useTodo();
-    const add=(e)=>{
+    const [todo, setTodo] = useState("");
+    const { addTodo } = useTodo();
+    const add = (e) => {
         e.preventDefault();
 
-        if(!todo) return;
+        if (!todo) return;
 
-        addTodo({todo:todo,completed:false});
+        addTodo({ todo: todo, completed: false });
         setTodo("");
     }
 
@@ -19,7 +19,7 @@ function TodoForm() {
                 type="text"
                 placeholder="Write Todo..."
                 value={todo}
-                onChange={(e)=>{
+                onChange={(e) => {
                     setTodo(e.target.value)
                 }}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
@@ -29,4 +29,4 @@ function TodoForm() {
             </button>
         </form>
     );
-}export default TodoForm;
+} export default TodoForm;
--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,20 +1,20 @@
-import { createContext,useContext } from "react";
+import { createContext, useContext } from "react";
 
-export const TodoContext=createContext({
-    todos:[
+export const TodoContext = createContext({
+    todos: [
         {
-            id:1,
-            todo:'here the message',
-            completed:false
+            id: 1,
+            todo: 'here the message',
+            completed: false
         }
     ],
-    addTodo:(todo)=>{},
-    updateTodo:(id,todo)=>{},
-    deleteTodo:(id)=>{},
-    toggleComplete:(id)=>{}
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { }
 });
 
-export const useTodo=()=>{
+export const useTodo = () => {
     return useContext(TodoContext);
 }
-export const TodoProvider=TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider;
